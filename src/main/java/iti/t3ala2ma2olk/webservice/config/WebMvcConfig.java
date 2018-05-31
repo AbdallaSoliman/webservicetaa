2a4167bf18bb2047bad50ac5aa6b1ce214226052
AbdallaSoliman <36964906+AbdallaSoliman@users.noreply.github.com>
2018-05-31 23:54:23 +0200

    profileerwer22 commit

--- src/main/java/iti/t3ala2ma2olk/webservice/config/WebMvcConfig.java
+++ src/main/java/iti/t3ala2ma2olk/webservice/config/WebMvcConfig.java
@@ -20,11 +20,6 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
     private final ApplicationContext applicationContext;
     private final EntityManager entityManager;
 
-        @Bean
-    public BCryptPasswordEncoder passwordEncoder() {
-        BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
-        return bCryptPasswordEncoder;
-    }
     @Autowired
     public WebMvcConfig(ApplicationContext applicationContext, EntityManager entityManager) {
         this.applicationContext = applicationContext;
@@ -38,5 +33,9 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
         argumentResolvers.add(new DTOModelMapper(objectMapper, entityManager));
     }
 
-
+   // @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+        return bCryptPasswordEncoder;
+    }
 }
