d683049ad63524ce70be2a2d28492f5178878ca6
Ali-Alzantot <a.alzantot@gmail.com>
2018-06-18 14:25:41 -0700

    update question closed

--- src/main/java/iti/t3ala2ma2olk/webservice/businesslayer/service/QuestionService.java
+++ src/main/java/iti/t3ala2ma2olk/webservice/businesslayer/service/QuestionService.java
@@ -90,6 +90,7 @@ public class QuestionService {
 
         question.setQuestionId(null);
         question.setIsdeleted(0);
+        question.setClosed(0);
         question.setQuestionDate(new Timestamp(Calendar.getInstance().getTime().getTime()));
         question.setVerified(0);
         //   Question  has been added successfully
